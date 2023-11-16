d6cd1eb828c9f82893332de98223ae7fed151e37
Kimnabin <ducvucong01@gmail.com>
2023-11-16 12:41:46 +0900

    itemdisplay done

--- router/rootRouter.js
+++ router/rootRouter.js
@@ -11,4 +11,12 @@ router.get("/shop/:category", (req, res) => {
   shop.home(req, res);
 });
 
+router.post("/shop/search", (req, res) => {
+  shop.search(req, res);
+});
+
+router.get("/shop/detail/:mer_id", (req, res) => {
+  shop.detail(req, res);
+});
+
 module.exports = router;
